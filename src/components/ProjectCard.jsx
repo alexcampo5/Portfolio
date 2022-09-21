eff1ca7f60f26b8937838de5969501c4d08d8e51
Alex Campo <abrettc@gmail.com>
2022-09-21 12:09:52 -0500

    updated react to get netlify deploy pt 3

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ export default function ProjectCard(props) {
       <img src={props.screenshot} alt='screenshot' className="screenshot"/>
       <h3>{props.title}</h3>
       <p>{props.description}</p>
-      <a href={props.deployment} target='_blank'>Deployment Link</a>
+      <a href={props.deployment} target='_blank' rel="noreferrer">Deployment Link</a>
       <div className="stack-container">
       {props.stack.map((tech) => (
         <div>
--- src/components/SocialsBar.jsx
+++ src/components/SocialsBar.jsx
@@ -6,9 +6,9 @@ export default function SocialsBar () {
   return (
     <div className="socialbar">
       <div className="social-bar-container">
-        <a href="https://www.linkedin.com/in/alexcampo5/" target="_blank"><img src={linkedin} alt='linkedin' className='social-icon'/></a>
-        <a href='https://github.com/alexcampo5' target='_blank'><img src={github} alt='github' className='social-icon'/></a>
-        <img src={email} alt="email" className='social-icon'/>
+        <a href="https://www.linkedin.com/in/alexcampo5/" target="_blank" rel="noreferrer"><img src={linkedin} alt='linkedin' className='social-icon'/></a>
+        <a href='https://github.com/alexcampo5' target='_blank' rel="noreferrer"><img src={github} alt='github' className='social-icon'/></a>
+        <img src={email} alt="email" className='social-icon' id='email'/>
       </div>
     </div>
   )
