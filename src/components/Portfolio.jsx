3545618143606941b4ef6426bad622f611c561da
Alex Campo <abrettc@gmail.com>
2022-09-21 17:07:20 -0500

    added screenshots to all projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,9 +5,10 @@ import react from '../assets/react (1).png'
 import html from '../assets/html-5.png'
 import css from '../assets/css-3.png'
 import js from '../assets/js.png'
-// import express from '../assets/express.png'
 import sql from '../assets/database.png'
 import mongo from '../assets/pngwing.com.png'
+import soccer from '../assets/soccerdb.png'
+import thriftr from '../assets/thriftrhome.png'
 
 export default function Portfolio () {
 
@@ -21,14 +22,14 @@ export default function Portfolio () {
     },
     {
       title: 'World Soccer DB',
-      screenshot: '',
+      screenshot: soccer,
       description: 'A quick database client built in a week designed to help soccer fans keep track of their favorite players.',
       deployment: '',
       stack: [react, js, mongo]
     },
     {
       title: 'thriftR',
-      screenshot: '',
+      screenshot: thriftr,
       description: `A social media app designed for people to share their thrift finds. Group project where I designed the structure of the front end including CRUD functionality.`,
       deployment: '',
       stack: [react, js, sql]
--- src/components/SocialsBar.jsx
+++ src/components/SocialsBar.jsx
@@ -1,6 +1,6 @@
 import github from '../assets/github-sign.png'
 import linkedin from '../assets/linkedin.png'
-import email from '../assets/email.png'
+// import email from '../assets/email.png'
 
 export default function SocialsBar () {
   return (
@@ -8,7 +8,7 @@ export default function SocialsBar () {
       <div className="social-bar-container">
         <a href="https://www.linkedin.com/in/alexcampo5/" target="_blank" rel="noreferrer"><img src={linkedin} alt='linkedin' className='social-icon'/></a>
         <a href='https://github.com/alexcampo5' target='_blank' rel="noreferrer"><img src={github} alt='github' className='social-icon'/></a>
-        <img src={email} alt="email" className='social-icon' id='email'/>
+        {/* <img src={email} alt="email" className='social-icon' id='email'/> */}
       </div>
     </div>
   )
